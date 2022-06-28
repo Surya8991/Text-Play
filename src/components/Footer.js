2552d274cff2707caad4450b28e307c0c9ba1505
SURYA L <Suryaraj8147@gmail.com>
2022-06-28 16:22:36 +0530

    Social Icons changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer(props) {
        <div
         className={`container text-${props.mode === "dark" ? "light" : "dark"}`}
       >
-    <a className={`text-${props.mode === "dark" ? "light" : "dark"}`} href='https://twitter.com/SURYA_L1998'><i className="social-icon fab fa-twitter fa-2x"></i></a>
-    <a className={`text-${props.mode === "dark" ? "light" : "dark"}`} href='https://www.linkedin.com/in/surya-l/'><i className="social-icon fab fa-linkedin fa-2x"></i></a>
-    <a className={`text-${props.mode === "dark" ? "light" : "dark"}`} href='https://github.com/Surya8991'><i className="social-icon fab fa-github fa-2x"></i></a>
+    <a className={`text-${props.mode === "dark" ? "light" : "dark"}`} href='https://twitter.com/SURYA_L1998' target="_blank" rel="noopener noreferrer"><i className="social-icon fab fa-twitter fa-2x"></i></a>
+    <a className={`text-${props.mode === "dark" ? "light" : "dark"}`} href='https://www.linkedin.com/in/surya-l/' target="_blank" rel="noopener noreferrer"><i className="social-icon fab fa-linkedin fa-2x"></i></a>
+    <a className={`text-${props.mode === "dark" ? "light" : "dark"}`} href='https://github.com/Surya8991' target="_blank" rel="noopener noreferrer"><i className="social-icon fab fa-github fa-2x"></i></a>
     <h5>© Copyright 2022-{new Date().getFullYear()} - Surya L</h5>
     </div>
   </footer>
