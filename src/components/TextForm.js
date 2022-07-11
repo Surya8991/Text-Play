ecc4c6a5b5f3215003d68eadd888c9c4615e1625
SURYA L <Suryaraj8147@gmail.com>
2022-07-11 19:38:03 +0530

    bug fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,9 +35,7 @@ export default function TextForm(props) {
   };
   //To extract the mail id from the text.
   const handlemailExtract = () => {
-    let emailIds = text.match(
-      /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
-    );
+    let emailIds = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
     if (emailIds != null) {
       setText(emailIds);
       props.showAlert("Extracted Email Ids from the text", "success");
@@ -113,151 +111,153 @@ export default function TextForm(props) {
       props.showAlert("No text to download", "warning");
     }
   }
-//   //To speak the text and stop on click.
-//   const handleSpeak = () => {
-// if(speechSynthesis.speaking)
-//     {
-//       window.speechSynthesis.cancel();
-//       props.showAlert("Text is Stoped", "success");
-//     }else if (text.length !== 0) {
-//       var msg = new SpeechSynthesisUtterance(text);
-//       window.speechSynthesis.speak(msg);
-//       props.showAlert("Text is spoken", "success");
-//     } else {
-//       props.showAlert("No text to speak", "warning");
-//     }
-//     }
-//   //Program to execute clear button when we move to another page
-//   window.onbeforeunload = function () { 
-//     window.speechSynthesis.cancel();
-//   }
-    const [text, setText] = useState("");
-    return (
-      <>
-        <div
-          className="container"
-          id="home"
-          style={{ color: props.mode === 'dark' ? 'white' : '#06283D' }}
-        >
-          <h2 className="mb-4">{props.heading}</h2>
-          <h5 className="mb-4">{props.des}</h5>
-          <div className="mb-3">
-            {/* Text area where you enter the text */}
-            <textarea
-              className="form-control"
-              style={{
-                backgroundColor: props.mode === "dark" ? "#06283D" : "white",
-                color: props.mode === "dark" ? "white" : "#06283D",
-              }}
-              value={text}
-              onChange={handleChange}
-              id="myForm"
-              rows="6"
-              spellCheck="false"
-            ></textarea>
-          </div>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleUpperCase}
-            disabled={text.length === 0}
-          >
-            Converting to Upper Case
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleLowerCase}
-            disabled={text.length === 0}
-          >
-            Converting to Lower Case
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleclearText}
-            disabled={text.length === 0}
-          >
-            Clear Text
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleCopy}
-            disabled={text.length === 0}
-          >
-            Copy Text
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleCut}
-            disabled={text.length === 0}
-          >
-            Text For twitter
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={capitalization}
-            disabled={text.length === 0}
-          >
-            Capitalize each word
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handlemailExtract}
-            disabled={text.length === 0}
-          >
-            Extract Emails
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handletextExtract}
-            disabled={text.length === 0}
-          >
-            Extract Text
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleNumExtract}
-            disabled={text.length === 0}
-          >
-            Extract Numbers
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleExtraSpaces}
-            disabled={text.length === 0}
-          >
-            Remove Extra Spaces
-          </button>
-          <button
-            className="btn btn-dark mx-2 my-2"
-            onClick={handleRevClick}
-            disabled={text.length === 0}
-          >
-            Reverse Text
-          </button>
-          <button
-            className="btn btn-lg btn-dark mx-2 my-2"
-            onClick={handleDownload}
-            disabled={text.length === 0}
-          >
-            Download Text
-          </button>
-          {/* <button type="submit" onClick={handleSpeak} className="btn btn-lg btn-dark mx-2 my-2" disabled={text.length === 0}>Speak</button> */}
+  //   //To speak the text and stop on click.
+  //   const handleSpeak = () => {
+  // if(speechSynthesis.speaking)
+  //     {
+  //       window.speechSynthesis.cancel();
+  //       props.showAlert("Text is Stoped", "success");
+  //     }else if (text.length !== 0) {
+  //       var msg = new SpeechSynthesisUtterance(text);
+  //       window.speechSynthesis.speak(msg);
+  //       props.showAlert("Text is spoken", "success");
+  //     } else {
+  //       props.showAlert("No text to speak", "warning");
+  //     }
+  //     }
+  //   //Program to execute clear button when we move to another page
+  //   window.onbeforeunload = function () { 
+  //     window.speechSynthesis.cancel();
+  //   }
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === 'dark' ? 'white' : '#06283D' }}
+      >
+        <h2 className="mb-4">{props.heading}</h2>
+        <h5 className="mb-4">{props.des}</h5>
+        <div className="mb-3">
+          {/* Text area where you enter the text */}
+          <textarea
+            className="form-control"
+            style={{
+              backgroundColor: props.mode === "dark" ? "#06283D" : "white",
+              color: props.mode === "dark" ? "white" : "#06283D",
+            }}
+            value={text}
+            onChange={handleChange}
+            id="myForm"
+            rows="6"
+            spellCheck="false"
+          ></textarea>
         </div>
-        <div
-          className={`container text-${props.mode === "dark" ? "light" : "dark"}`}
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleUpperCase}
+          disabled={text.length === 0}
         >
-          <h2 className="my-3">Text Summary</h2>
-          {/* To display the word and characters count */}
-          <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters.</p>
-          <p>{Math.floor((0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length)*100)/100} Minutes read</p>
-          <p>It takes {Math.floor(((text.split(/\s+/).filter((element) => { return element.length !== 0 }).length)*0.00714285714)*100)/100} minutes to Speak</p>
-          {/* //To display the text preview.*/}
-          <h2 className="my-3">Text Preview</h2>
-          <p>{text.length > 0 ? text : "Nothing to preview"}</p>
-        </div>
+          Converting to Upper Case
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleLowerCase}
+          disabled={text.length === 0}
+        >
+          Converting to Lower Case
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleclearText}
+          disabled={text.length === 0}
+        >
+          Clear Text
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleCopy}
+          disabled={text.length === 0}
+        >
+          Copy Text
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleCut}
+          disabled={text.length === 0}
+        >
+          Text For twitter
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={capitalization}
+          disabled={text.length === 0}
+        >
+          Capitalize each word
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handlemailExtract}
+          disabled={text.length === 0}
+        >
+          Extract Emails
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handletextExtract}
+          disabled={text.length === 0}
+        >
+          Extract Text
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleNumExtract}
+          disabled={text.length === 0}
+        >
+          Extract Numbers
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleExtraSpaces}
+          disabled={text.length === 0}
+        >
+          Remove Extra Spaces
+        </button>
+        <button
+          className="btn btn-dark mx-2 my-2"
+          onClick={handleRevClick}
+          disabled={text.length === 0}
+        >
+          Reverse Text
+        </button>
+        <button
+          className="btn btn-lg btn-dark mx-2 my-2"
+          onClick={handleDownload}
+          disabled={text.length === 0}
+        >
+          Download Text
+        </button>
+        {/* <button type="submit" onClick={handleSpeak} className="btn btn-lg btn-dark mx-2 my-2" disabled={text.length === 0}>Speak</button> */}
+      </div>
+      <div
+        className={`container text-${props.mode === "dark" ? "light" : "dark"}`}
+      >
+        <h2 className="my-3">Text Summary</h2>
+        {/* To display the word and characters count */}
+        <p>Your Text has <strong>{text.length}</strong> characters. It takes{" "}
+          <strong>
+            {Math.floor((0.008 *
+              text.length)*100)/100}
+          </strong>{" "}
+          Minutes to read the text.
+        </p>
+        {/* //To display the text preview.*/}
+        <h2 className="my-3">Text Preview</h2>
+        <p>{text.length > 0 ? text : "Nothing to preview"}</p>
+      </div>
 
-
-        {/* To Host in Github Page */}
-        {/* <div
+      {/* To Host in Github Page */}
+      {/* <div
       className={`container text-${props.mode === "dark" ? "light" : "dark"}`}
       id="about"
     >
@@ -415,6 +415,6 @@ export default function TextForm(props) {
     </div>
     About Section Ends here */}
 
-      </>
-    );
-  }
+    </>
+  );
+}
